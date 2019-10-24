23d58dea3d23c3c1c2ea366d64ad50f110bc5ee6
fred belotte <fredbelotte@outlook.com>
2019-10-24 15:34:16 -0500

    add helloworld solution

---
+++ 00_csharp/HelloWorld/HelloWorld.Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using sc = System.Console; //import with alias
+
+namespace HelloWorld.Client
+{
+  /// <summary>
+  /// contains the start of the program
+  /// </summary>
+  internal class Program
+  {
+    /// <summary>
+    /// starts the program
+    /// </summary>
+    private static void Main()
+    {
+      sc.WriteLine("Hello World!");
+    }
+  }
+}

62db64427a49ef236a8f5736f4f84ee13409cfd4
Roberto <rlguzman.developer@gmail.com>
2019-10-28 23:02:41 -0700

    Added Singleton Pattern for Audio/Video Player

--- 00_cSharp/MediaWorld/MediaWorld.Domain/Singletons/AudioPlayerSingleton.cs
+++ 00_cSharp/MediaWorld/MediaWorld.Domain/Singletons/AudioPlayerSingleton.cs
@@ -1,3 +1,5 @@
+using System;
+using MediaWorld.Domain.Abstracts;
 using MediaWorld.Domain.Interfaces;
 
 namespace MediaWorld.Domain.Singletons
@@ -5,5 +7,22 @@ namespace MediaWorld.Domain.Singletons
   /// <summary>
   /// contains the singleton pattern
   /// </summary>
-  public class AudioPlayerSingleton : IPlayer {}
+  public class AudioPlayerSingleton : IPlayer {
+
+    private static readonly AudioPlayerSingleton _instance = new AudioPlayerSingleton();
+
+    public static AudioPlayerSingleton Instance 
+    {
+      get {
+        return _instance;
+      }
+    }
+
+    private AudioPlayerSingleton() {}
+
+    public void Execute(string command, AAudio audio)
+    {
+      Console.WriteLine(audio);
+    }
+  }
 }
--- 00_cSharp/MediaWorld/MediaWorld.Domain/Singletons/VideoPlayerSingleton.cs
+++ 00_cSharp/MediaWorld/MediaWorld.Domain/Singletons/VideoPlayerSingleton.cs
@@ -1,3 +1,5 @@
+using System;
+using MediaWorld.Domain.Abstracts;
 using MediaWorld.Domain.Interfaces;
 
 namespace MediaWorld.Domain.Singletons
@@ -5,5 +7,24 @@ namespace MediaWorld.Domain.Singletons
   /// <summary>
   /// contains the singleton pattern
   /// </summary>
-  public class VideoPlayerSingleton : IPlayer {}
+  public class VideoPlayerSingleton : IPlayer 
+  {
+
+    private static readonly VideoPlayerSingleton _instance = new VideoPlayerSingleton();
+
+    public static VideoPlayerSingleton Instance 
+    {
+      get 
+      {
+        return _instance;
+      }
+    }
+
+    private VideoPlayerSingleton() {}
+
+    public void Execute(string command, AVideo video)
+    {
+      Console.WriteLine(video);
+    }
+  }
 }
